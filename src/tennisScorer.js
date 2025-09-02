50a437961ca5c8a1ace3288e80d9832b9c03090d
Ostin <ostinrivas15@gmail.com>
2025-09-02 00:46:22 -0400

    Refactorizacion de codigo, hacelo lo mismo pero mejor

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -1,7 +1,9 @@
 class TennisScorer {
-  constructor() {
-    this.player1Points = 0;
+  constructor(player1Points, player2Points = 0) {
+    this.player1Points = player1Points;
+    this.player2Points = player2Points;
   }
+  /*
   showScore() {
     if (this.player1Points === 1) {
       return "15 - Love";
@@ -11,9 +13,18 @@ class TennisScorer {
       return "40 - Love";
     }
     return "Love - Love";
-  }
+  }*/
+ 
+    showScore() {
+      const res = ["Love", "15", "30", "40"];
+      return res[this.player1Points] + " - " + res[this.player2Points];
+    }
+
   player1Scores() {
     this.player1Points++;
   }
 }
+
+
+
 export default TennisScorer;
--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -3,24 +3,24 @@ import TennisScorer from "./tennisScorer.js";
 describe("TennisScorer", () => {
   //Empieza sin puntos     --> "Love - Love"
   it("game empieza con ambos en cero", () => {
-    const scorer = new TennisScorer();
+    const scorer = new TennisScorer(0);
     expect(scorer.showScore()).toEqual("Love - Love");
   });
 
   it("Jugador 1 marca un punto y el jugador en cero", () => {
-    const scorer = new TennisScorer();
+    const scorer = new TennisScorer(0);
     scorer.player1Scores();
     expect(scorer.showScore()).toEqual("15 - Love");
   });
 
   it("Jugador 1 marca 2 puntos y jugador 2 en cero", () => {
-    const scorer = new TennisScorer();
+    const scorer = new TennisScorer(0);
     scorer.player1Scores();
     scorer.player1Scores();
     expect(scorer.showScore()).toEqual("30 - Love");
   });
   it("El Jugador 1 marca 3 puntos y jugador 2 en 0", () => {
-    const scorer = new TennisScorer();
+    const scorer = new TennisScorer(0);
     scorer.player1Scores();
     scorer.player1Scores();
     scorer.player1Scores();
