a01624ba1fc9752d449e657f3c8c6eef86ba13d3
Ostin <ostinrivas15@gmail.com>
2025-09-04 01:17:11 -0400

    Implementacion completa

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,24 @@
-import sumar from "./sumador";
+import TennisScorer from "./tennisScorer.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const scoreDiv = document.querySelector("#score-div");
+const player1Btn = document.querySelector("#player1-btn");
+const player2Btn = document.querySelector("#player2-btn");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const scorer = new TennisScorer(0, 0);
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+function updateScore() {
+  scoreDiv.textContent = scorer.showScore();
+}
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+player1Btn.addEventListener("click", () => {
+  scorer.player1Scores();
+  updateScore();
 });
+
+player2Btn.addEventListener("click", () => {
+  scorer.player2Scores();
+  updateScore();
+});
+
+// Mostrar marcador inicial
+updateScore();
